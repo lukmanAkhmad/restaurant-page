b194e5c7f4706e1014eb723d6ba7cdbb8197824d
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2024-05-09 07:43:12 +0700

    membuat kode lebih clean atau DRY

--- src/contact.js
+++ src/contact.js
@@ -1,19 +1,44 @@
 // ini module
-export function createContactPage(){
+export function createContactPage() {
     const divContent = document.querySelector('#content');
 
     // clear #content section
     divContent.replaceChildren();
 
     divContent.appendChild(createHeaderContent());
-    divContent.appendChild(createContactContentOne());
-    divContent.appendChild(createContactContentTwo());
-    divContent.appendChild(createContactContentThree());
+
+    content.forEach((item) => {
+
+        const divContactContent = document.createElement('div');
+        divContactContent.classList.add('contact-content');
+
+        const h3Person = document.createElement('h3');
+        h3Person.classList.add('person');
+        h3Person.textContent = item.name;
+        divContactContent.appendChild(h3Person);
+
+        const paraPosition = document.createElement('p');
+        paraPosition.classList.add('position');
+        paraPosition.textContent = item.position;
+        divContactContent.appendChild(paraPosition);
+
+        const paraPhone = document.createElement('p');
+        paraPhone.classList.add('phone');
+        paraPhone.textContent = item.phone;
+        divContactContent.appendChild(paraPhone);
+
+        const paraEmail = document.createElement('p');
+        paraEmail.classList.add('email');
+        paraEmail.textContent = item.email;
+        divContactContent.appendChild(paraEmail);
+
+        divContent.appendChild(divContactContent);
+    });
 
     return;
 }
 
-function createHeaderContent(){
+function createHeaderContent() {
     const divContactHeaderContent = document.createElement('div');
     divContactHeaderContent.classList.add('contact-header-content');
 
@@ -25,83 +50,17 @@ function createHeaderContent(){
     return divContactHeaderContent;
 }
 
-function createContactContentOne(){
-    const divContactContent = document.createElement('div');
-    divContactContent.classList.add('contact-content');
-
-    const h3Person = document.createElement('h3');
-    h3Person.classList.add('person');
-    h3Person.textContent = 'Mama Bear';
-    divContactContent.appendChild(h3Person);
-
-    const paraPosition = document.createElement('p');
-    paraPosition.classList.add('position');
-    paraPosition.textContent = 'Cheff';
-    divContactContent.appendChild(paraPosition);
-
-    const paraPhone = document.createElement('p');
-    paraPhone.classList.add('phone');
-    paraPhone.textContent = '555-555-5554';
-    divContactContent.appendChild(paraPhone);
-
-    const paraEmail = document.createElement('p');
-    paraEmail.classList.add('email');
-    paraEmail.textContent = 'totallyRealEmail@notFake.com';
-    divContactContent.appendChild(paraEmail);
-
-    return divContactContent;
+class Contact {
+    constructor(name, position, phone, email) {
+        this.name = name;
+        this.position = position;
+        this.phone = phone;
+        this.email = email;
+    }
 }
 
-function createContactContentTwo(){
-    const divContactContent = document.createElement('div');
-    divContactContent.classList.add('contact-content');
-
-    const h3Person = document.createElement('h3');
-    h3Person.classList.add('person');
-    h3Person.textContent = 'Papa Bear';
-    divContactContent.appendChild(h3Person);
-
-    const paraPosition = document.createElement('p');
-    paraPosition.classList.add('position');
-    paraPosition.textContent = 'Manager';
-    divContactContent.appendChild(paraPosition);
-
-    const paraPhone = document.createElement('p');
-    paraPhone.classList.add('phone');
-    paraPhone.textContent = '555-555-5555';
-    divContactContent.appendChild(paraPhone);
-
-    const paraEmail = document.createElement('p');
-    paraEmail.classList.add('email');
-    paraEmail.textContent = 'perfectlyRealEmail@notFake.com';
-    divContactContent.appendChild(paraEmail);
-
-    return divContactContent;
-}
-
-function createContactContentThree(){
-    const divContactContent = document.createElement('div');
-    divContactContent.classList.add('contact-content');
-
-    const h3Person = document.createElement('h3');
-    h3Person.classList.add('person');
-    h3Person.textContent = 'Baby Bear';
-    divContactContent.appendChild(h3Person);
-
-    const paraPosition = document.createElement('p');
-    paraPosition.classList.add('position');
-    paraPosition.textContent = 'Waiter';
-    divContactContent.appendChild(paraPosition);
-
-    const paraPhone = document.createElement('p');
-    paraPhone.classList.add('phone');
-    paraPhone.textContent = '555-555-5556';
-    divContactContent.appendChild(paraPhone);
-
-    const paraEmail = document.createElement('p');
-    paraEmail.classList.add('email');
-    paraEmail.textContent = 'perfectlyRealEmail@notFake.com';
-    divContactContent.appendChild(paraEmail);
-
-    return divContactContent;
-}
+let content = [
+    new Contact('Mama Bear', 'Cheff', '555-555-5554', 'totallyRealEmail@notFake.com'),
+    new Contact('Papa Bear', 'Manager', '555-555-5555', 'perfectlyRealEmail@notFake.com'),
+    new Contact('Baby Bear', 'Waiter', '555-555-5556', 'perfectlyRealEmail@notFake.com'),
+];

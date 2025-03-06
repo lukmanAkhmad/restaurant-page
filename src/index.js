56d2526a96e500e13fceec91e0a854ddbee33deb
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2025-03-07 05:39:22 +0700

    chore: delete comment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import "./style.css";
-// import odinImage from "./odin.png";
-// import {greeting} from "./greeting";
 import { screenController } from "./uiControl";
+
 screenController();
-
-// const image = document.createElement("img");
-// image.src = odinImage;
-
-// document.body.appendChild(image);
-
-
-// console.log(greeting);
